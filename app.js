a68c7be329a529362b19b8583410c05a315e4b17
SamuraySC <samurayiskl@gmail.com>
2022-03-24 19:57:58 +0300

    can we hit 500 likes before i hit 100 commits

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-const session = require("express-session")
+const session = require("cookie-session")
 const passport = require("passport");
 const cookieParser = require("cookie-parser")
 const bcrypt = require("bcryptjs")
