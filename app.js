8ac84c60300ec1fa4b8cd9dd52f673ab258eb479
SamuraySC <samurayiskl@gmail.com>
2022-03-26 05:34:32 +0300

    bad practice in cors2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
-app.use(cors())
+app.use(cors());
 // app.use(cors({origin:'https://luminous-belekoy-e89225.netlify.app/', credentials:true}))
 app.use(session({ 
     secret: "testing secret 123", 
