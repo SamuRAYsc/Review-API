22ad4042ca6a326d12d11a614ae333f1dde0a7cd
SamuraySC <samurayiskl@gmail.com>
2022-03-27 19:34:35 +0300

    115

--- app.js
+++ app.js
@@ -63,19 +63,16 @@ app.post('/',(req,res) => {
     res.send('Server UP');
 });
 
-app.post('/login', passport.authenticate('local', {
-    successRedirect: '/loginSuccess',
-    failureRedirect: '/loginFailed',
-    }),(req, res) => {res.send(req.sessionID);}
+app.post('/login', passport.authenticate("local"),(req, res) => {res.send(req.sessionID);}
 );
-app.get('/loginSuccess', (req, res) => {
-    req.session.user = req.user;
-    req.session.save();
-    res.send(req.sessionID);
-});
-app.get('/loginFailed', (req, res) => {
-    res.send('Login Failed! Try again');
-});
+// app.get('/loginSuccess', (req, res) => {
+//     req.session.user = req.user;
+//     req.session.save();
+//     res.send(req.sessionID);
+// });
+// app.get('/loginFailed', (req, res) => {
+//     res.send('Login Failed! Try again');
+// });
 
 app.get('/user', (req, res) =>{
     res.send(req.user);
