73e28096aad4d004d46ca228c7b751d0895adc61
SamuraySC <samurayiskl@gmail.com>
2022-03-28 18:29:06 +0300

    try sorting2

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.get('/userreviews', async (req, res) =>{
     res.send(List);
 });
 app.get('/latestReviews', async (req, res) =>{
-    const List = await models.Review.findAll({ order: [ sequelize.fn('max', sequelize.col('updatedAt')) ],
+    const List = await models.Review.findAll({ order: [ ['updatedAt', "DESC"] ],
         limit: 2,
         attributes: ['id','name', 'description', 'updatedAt']
     });
